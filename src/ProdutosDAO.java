9d29e7dc2da308cbb442d5b49c848b4273aca2d6
Davi <daviremzetti@gmail.com>
2023-07-10 22:50:07 -0300

    Criado a funcao e a tela para listar produtos vendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -100,5 +100,33 @@ public class ProdutosDAO {
             JOptionPane.showMessageDialog(null, "Falha na conexão");
         }
     }
+    
+    
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() throws ClassNotFoundException {
+        conectaDAO conexao = new conectaDAO();
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+
+        try {
+            conn = conexao.connectDB();
+            prep = conn.prepareStatement("select * from produtos where status = 'Vendido'");
+            resultset = prep.executeQuery();
+            while (resultset.next()) {
+                int id = resultset.getInt("id");
+                String nome = resultset.getString("nome");
+                int valor = resultset.getInt("valor");
+                String status = resultset.getString("status");
+                ProdutosDTO novoProduto = new ProdutosDTO();
+                novoProduto.setId(id);
+                novoProduto.setNome(nome);
+                novoProduto.setValor(valor);
+                novoProduto.setStatus(status);
+                listagem.add(novoProduto);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Falha na consulta no banco de dados", "ERRO DE CONSULTA", JOptionPane.ERROR_MESSAGE);
+        }
+
+        return listagem;
+    }
 
 }
